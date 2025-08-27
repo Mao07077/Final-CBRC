11396b3c3c81aa35b7990753e55b4f6839643fde
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-08-27 09:37:38 +0800

    Update import in moduleStore.js to use axiosClient.js after file rename

--- src/store/instructor/moduleStore.js
+++ src/store/instructor/moduleStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import apiClient from '../../api/axios';
+import apiClient from '../../api/axiosClient';
 
 const useModuleStore = create((set, get) => ({
   modules: [],
