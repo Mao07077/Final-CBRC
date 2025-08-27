ab2ee42bd584e7b44c966cebec0bdf9bd5270d83
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-08-27 09:43:00 +0800

    Push all remaining changes

--- src/features/admin/adminDashboard/components/PerformanceGraph.jsx
+++ src/features/admin/adminDashboard/components/PerformanceGraph.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import apiClient from "../../../api/axios";
+import apiClient from "../../../api/axiosClient";
 
 const PerformanceGraph = () => {
   const [summary, setSummary] = useState(null);
