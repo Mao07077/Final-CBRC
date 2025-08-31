2577b4e541ddb90628cd6c5a5f972015c2f86093
Mao07077 <106759453+Mao07077@users.noreply.github.com>
2025-08-31 08:51:07 +0800

    Fix: remove duplicate PDFDownloadLink import for build success

--- src/pages/student/StudentDataPrintingPage.jsx
+++ src/pages/student/StudentDataPrintingPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-import StudentReportPDF from "../../features/admin/adminStudentPerformance/components/StudentReportPDF";
 import { PDFDownloadLink, Document, Page, Text, StyleSheet } from "@react-pdf/renderer";
+import StudentReportPDF from "../../features/admin/adminStudentPerformance/components/StudentReportPDF";
 import { FiFileText, FiDownload, FiPrinter } from "react-icons/fi";
 import apiClient from "../../api/axiosClient";
 
